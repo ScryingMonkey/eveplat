7711cbd027bf52e255cef2c4253efc3e180be070
ScryingMonkey <seekingmonkey@gmail.com>
2020-02-03 00:09:52 -0500

    fixed an issue with deleting events

--- src/contexts/Firebase/Firebase.ts
+++ src/contexts/Firebase/Firebase.ts
@@ -2,7 +2,6 @@ import { BehaviorSubject } from "rxjs";
 import app from "firebase/app";
 // import 'firebase/auth';
 import "firebase/firestore";
-// import "firebase/database";
 import firebaseConfig from "./firebaseConfig";
 
 class Firebase {
@@ -47,7 +46,12 @@ class Firebase {
     }
   };
 
-  subscribeToCollectionFromFirestore =(fb:Firebase,key:string,thing:any,updateFunc:any)=> {
+  subscribeToCollectionFromFirestore = (
+    fb: Firebase,
+    key: string,
+    thing: any,
+    updateFunc: any
+  ) => {
     fb.colls[key].ref.onSnapshot(res => {
       let arr = res.docs.map(doc => {
         let t = new thing();
@@ -57,32 +61,31 @@ class Firebase {
         return t;
       });
       // console.log('...arr after map');
-      // console.log(arr); 
+      // console.log(arr);
       updateFunc(arr);
       // console.log(`Update coll [${key}]`);
       // console.log(arr);
     });
-  }
+  };
 
   // const increment = firestore.FieldValue.increment(1);
 
-  addDoc = (collName:string, docId:string, doc:any) => {
+  addDoc = (collName: string, docId: string, doc: any) => {
     console.log(`...addDoc(${collName},${docId},doc)`);
     console.log(doc);
     this.colls[collName].ref.doc(docId).set(doc);
   };
-  deleteDoc = (collName, docId:string) => {
+  deleteDoc = (collName, docId: string) => {
     console.log(`deleteDoc(${collName},${docId})`);
     this.colls[collName].ref.doc(docId).delete();
   };
-  setDoc = (collName:string, docId:string, doc:any)=>{
+  setDoc = (collName: string, docId: string, doc: any) => {
     console.log(`...addDoc(${collName},${docId},doc)`);
     console.log(doc);
     this.colls[collName].ref.doc(docId).set(doc);
-  }
+  };
 }
-export {Firebase};
+export { Firebase };
 
 const fb = new Firebase();
 export default fb;
-
--- src/contexts/Global/GlobalReducer.tsx
+++ src/contexts/Global/GlobalReducer.tsx
@@ -1,78 +1,10 @@
-import {InitialState} from './InitialState';
+import { InitialState } from "./InitialState";
 import Venue from "../../types/Venue";
 import { TicketEvent } from "../../types/_index";
-import { DataObject } from '../../components/CbList/_index';
-import {CbRoute} from '../../types/_index';
+import { DataObject } from "../../components/CbList/_index";
+import { CbRoute } from "../../types/_index";
 
-export const globalReducer = (
-    state:InitialState, 
-    action: Action
-  ):InitialState => {
-
-  const nstate:InitialState = {...state};
-
-  switch (action.type) {
-    case ActionType.ADD_ROUTE:
-      nstate.routes =  [...nstate.routes, action.payload.route];
-      break;
-
-    case ActionType.SET_ITEM:
-      // nstate.event.events = [...nstate.event.events, action.payload.event];
-      const io = action.payload.dataObject.getObject();
-      action.payload.db.setDoc(action.payload.coll, io.id, io);
-      // clear 
-      
-    case ActionType.SET_EVENT:
-      // nstate.event.events = [...nstate.event.events, action.payload.event];
-      const teo = action.payload.event.getObject();
-      action.payload.db.setDoc("events", teo.id, teo);
-      // clear newTe
-      nstate.event.newTe = new TicketEvent();
-      break;
-    case ActionType.SET_EVENTS:
-      nstate.event.events = [...action.payload.events];
-      break;
-    case ActionType.DELETE_EVENT: 
-      action.payload.db.deleteDoc('events',action.payload.event);
-      nstate.event.events = [...nstate.event.events
-        .filter(e => e.id !== action.payload.id)
-      ];
-      break;
-    case ActionType.SET_NEW_TE:
-      nstate.event.newTe = action.payload.event;
-      break;
-    
-    case ActionType.SET_VENUE:
-      // nstate.venue.venues = [...nstate.venue.venues, action.payload.venue];
-      const vo = action.payload.venue.getObject();
-      action.payload.db.setDoc("venues", vo.id, vo)
-      // clear newV
-      nstate.venue.newV = new Venue();
-      break;
-    case ActionType.SET_VENUES:
-      nstate.venue.venues = [...action.payload.venues];
-      break;
-    case ActionType.DELETE_VENUE:
-      action.payload.db.deleteDoc('venues',action.payload.venue.id); 
-      nstate.venue.venues = [...nstate.venue.venues
-          .filter(v => v.id !== action.payload.venue.id)
-        ];
-      break;
-    case ActionType.SET_NEW_V:
-      nstate.venue.newV = action.payload.venue.clone();
-      break;
-    
-    default:
-      console.log(`...globalreducer(state, ${action}): reached default case.`);
-  }
-  
-  // console.log('nstate from reducer.UPDATE_NEW_TE');
-  // console.log(nstate);
-  return nstate;
-};
-export default globalReducer;
-
-export enum ActionType {  //move to global reducer?
+export enum ActionType { //move to global reducer?
   ADD_ROUTE = "ADD_ROUTE",
 
   SET_ITEM = "SET_ITEM",
@@ -88,12 +20,11 @@ export enum ActionType {  //move to global reducer?
   SET_VENUE = "SET_VENUE",
   SET_VENUES = "SET_VENUES",
   DELETE_VENUE = "DELETE_VENUE",
-  SET_NEW_V = "SET_NEW_V",
-
+  SET_NEW_V = "SET_NEW_V"
 }
 export type Payload = {
   db?: any;
-  coll?: 'events' | 'venues';
+  coll?: "events" | "venues";
   id?: string;
   route?: CbRoute;
   label?: string;
@@ -108,3 +39,75 @@ export type Action = {
   type: string;
   payload?: Payload;
 };
+
+export const globalReducer = (
+  state: InitialState,
+  action: Action
+): InitialState => {
+  const nstate: InitialState = { ...state };
+
+  switch (action.type) {
+    case ActionType.ADD_ROUTE:
+      nstate.routes = [...nstate.routes, action.payload.route];
+      break;
+
+    case ActionType.SET_ITEM:
+      // nstate.event.events = [...nstate.event.events, action.payload.event];
+      const io = action.payload.dataObject.getObject();
+      action.payload.db.setDoc(action.payload.coll, io.id, io);
+      // clear
+      break;
+
+    case ActionType.SET_EVENT:
+      // nstate.event.events = [...nstate.event.events, action.payload.event];
+      const teo = action.payload.event.getObject();
+      action.payload.db.setDoc("events", teo.id, teo);
+      // clear newTe
+      nstate.event.newTe = new TicketEvent();
+      break;
+
+    case ActionType.SET_EVENTS:
+      nstate.event.events = [...action.payload.events];
+      break;
+
+    case ActionType.DELETE_EVENT:
+      action.payload.db.deleteDoc("events", action.payload.event.id);
+
+      nstate.event.events = [
+        ...nstate.event.events.filter(e => e.id !== action.payload.event.id)
+      ];
+      break;
+
+    case ActionType.SET_NEW_TE:
+      nstate.event.newTe = action.payload.event;
+      break;
+
+    case ActionType.SET_VENUE:
+      // nstate.venue.venues = [...nstate.venue.venues, action.payload.venue];
+      const vo = action.payload.venue.getObject();
+      action.payload.db.setDoc("venues", vo.id, vo);
+      // clear newV
+      nstate.venue.newV = new Venue();
+      break;
+    case ActionType.SET_VENUES:
+      nstate.venue.venues = [...action.payload.venues];
+      break;
+    case ActionType.DELETE_VENUE:
+      action.payload.db.deleteDoc("venues", action.payload.venue.id);
+      nstate.venue.venues = [
+        ...nstate.venue.venues.filter(v => v.id !== action.payload.venue.id)
+      ];
+      break;
+    case ActionType.SET_NEW_V:
+      nstate.venue.newV = action.payload.venue.clone();
+      break;
+
+    default:
+      console.log(`...globalreducer(state, ${action}): reached default case.`);
+  }
+
+  // console.log('nstate from reducer.UPDATE_NEW_TE');
+  // console.log(nstate);
+  return nstate;
+};
+export default globalReducer;
